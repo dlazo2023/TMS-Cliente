a5b31e2bb2b4b3b3308d90ae9f29afaaba61f600
Dariel <dariellazo97@gmail.com>
2025-03-14 05:35:52 -0400

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,38 @@
 <template>
   <header>
     <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-md-8">
+      <div class="col-md-4 pe-0">
+        <div
+          style="background-color: #00235a; border-bottom-right-radius: 151px"
+        >
+          <img src="/public/Rectangle 2.png" alt="" class="fondologo" />
+          <img src="/src/assets/logo.png" alt="" class="logo" />
+          <div
+            class="yellow img-fluid d-flex align-items-center text-white text-center justify-content-center"
+          >
+            <i class="fas fa-phone-alt" style="font-size: 24px"></i>
+            <span>+18882079275</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-8 px-0">
         <div class="row" style="background-color: #00235a">
           <div class="col-md-12">
             <div class="top row py-3">
-              <div class="col-md-3">
+              <div class="col-md-4">
                 <span class="adresstime">
-                  <i class="fa-brands fa-facebook-f"></i>
+                  <i class="fas fa-map fa-1x me-2 text-warning"></i>
                   2500 Southbranch Blvd A, United States</span
                 >
               </div>
-              <div class="col-md-3">
+              <div class="col-md-4">
                 <span class="adresstime">
-                  Mon - Sat: 8 am - 5 pm, Sunday: CLOSED</span
+                  <i class="fas fa-clock fa-1x me-2 text-warning"></i> Mon -
+                  Sat: 8 am - 5 pm, Sunday: CLOSED</span
                 >
               </div>
 
-              <div class="col-md-6 d-flex justify-content-center text-center">
+              <div class="col-md-4 d-flex justify-content-center text-center">
                 <div class="d-flex align-items-center">
                   <a href="#" class="text-white me-3"
                     ><i class="fa-brands fa-facebook-f"></i
@@ -38,7 +52,7 @@
           </div>
 
           <div class="col-md-12 px-0">
-            <nav class="navbar navbar-expand-lg navbar-dark py-2">
+            <nav class="navbar navbar-expand-lg navbar-dark py-2 px-0">
               <div class="container">
                 <button
                   class="navbar-toggler"
@@ -99,6 +113,25 @@
 </script>
 
 <style scoped>
+  .yellow {
+    background-image: url("/public/Rectangle\ 4.png");
+    height: 42px;
+    background-repeat: no-repeat;
+    position: absolute;
+    left: 0;
+    top: 6rem;
+    width: 402px;
+    background-size: contain;
+  }
+  .logo {
+    position: absolute;
+    left: calc(100% - 78%);
+    max-height: 120px;
+  }
+  .fondologo {
+    width: 100%;
+    height: 146px;
+  }
   /* Estilos para la barra superior */
   .top-bar {
     background-color: #002f6c; /* Ajusta el color a tu gusto */
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
